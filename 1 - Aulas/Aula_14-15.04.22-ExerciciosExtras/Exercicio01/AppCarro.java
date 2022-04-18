a6a6643af15300ee2b30704f79336da3ac60c8b8
João Pedro Dias Caparroz - 125111374422 <125111374422@ulife.com.br>
2022-04-18 18:52:07 -0300

    Fazendo exercicio carro

--- 1 - Aulas/Aula_14-15.04.22-ExerciciosExtras/Exercicio01/AppCarro.java
+++ 1 - Aulas/Aula_14-15.04.22-ExerciciosExtras/Exercicio01/AppCarro.java
@@ -1,11 +1,34 @@
 package Exercicio01;
 
+import java.util.Scanner;
+
 public class AppCarro {
     public static void main(String[] args) {
         
-        Carro carro1 = new Carro("Volks", "Corsa", 15, 7, 15000);
+        Scanner scanner = new Scanner(System.in);
+        Carro carroUsado = new Carro("Volks", "Corsa", 15, 7, 15000);
 
-        System.out.println(carro1.Exibir());
+        System.out.println(carroUsado.exibir());
+
+        System.out.println("Deseja abastecer o carro? (s/n)");
+        char resposta = scanner.next().charAt(0);
+
+        if (resposta == 's') {
+
+            System.out.println("Quantos litros?");
+            double litros = scanner.nextDouble();
+
+            carroUsado.abastecer(litros);
+        }
+
+        carroUsado.exibir();
+        
+        System.out.println("Quantos km's você deseja percorrer?");
+        double distancia = scanner.nextDouble();
+
+        carroUsado.andar(distancia);
+
+        scanner.close();
     }
     
 }
--- 1 - Aulas/Aula_14-15.04.22-ExerciciosExtras/Exercicio01/Carro.java
+++ 1 - Aulas/Aula_14-15.04.22-ExerciciosExtras/Exercicio01/Carro.java
@@ -4,30 +4,61 @@ public class Carro {
 
     private String marca;
     private String modelo;
-    private double qntcombustivel;
+    private double qntCombustivel;
     private double consumo;
     private float quilometragem;
-    
-    public Carro(String marca, String modelo, double qntcombustivel, double consumo, float quilometragem){
+
+    public Carro(String marca, String modelo, double qntCombustivel, double consumo, float quilometragem) {
 
         this.marca = marca;
         this.modelo = modelo;
-        this.qntcombustivel = qntcombustivel;
+        this.qntCombustivel = qntCombustivel;
         this.consumo = consumo;
         this.quilometragem = quilometragem;
     }
 
-    public Carro(String marca, String modelo, double consumo){
+    public Carro(String marca, String modelo, double consumo) {
 
         this.marca = marca;
         this.modelo = modelo;
         this.consumo = consumo;
+
     }
 
-    public String Exibir() {
+    public String exibir() {
 
-        String exibirCarro = "Marca: " + marca + "Modelo: " + modelo + "Quantidade de Combustivel: " + qntcombustivel + "L " + "Consumo Médio: " + consumo + "km/l " + "Quilometragem: " + quilometragem + "km";
+        String exibirCarro = "Marca: " + marca + " | Modelo: " + modelo + " | Quantidade de Combustivel: "
+                + qntCombustivel + "L " + " | Consumo Médio: " + consumo + "km/l " + " | Quilometragem: "
+                + quilometragem + "km";
 
-        return exibirCarro;        
+        return exibirCarro;
+    }
+
+    public void abastecer(double litros) {
+
+        if(litros > 0){
+
+            System.out.println("Abastecido com sucesso!");
+            System.out.println("Adicionados " + litros + "L " + "ao tanque.");
+            System.out.println(litros + qntCombustivel + "L no tanque.");
+
+        } else {
+
+            System.out.println("Combustivel não pode ser negativo! =(");
+        }
+        
+    }
+
+    public void andar(double distancia) {
+
+        if (distancia > (qntCombustivel * consumo)) {
+
+            System.out.println("Viagem feita com sucesso! =)");
+
+        } else {
+
+            System.out.println("Não possue combustivel suficiente =(");
+        }
+        
     }
 }
